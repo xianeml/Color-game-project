64ffdff4b44088ebc838c060cf937c491d2f3a36
미현 <douob0@naver.com>
2020-08-09 20:49:58 +0900

    Generate six random colors

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,5 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
+
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -22,6 +16,7 @@ for(var i = 0; i < squares.length; i++){
         //grab color of clicked square
         var clickedColor = this.style.backgroundColor;
         //compare color to picked color
+        console.log(clickedColor, pickedColor);
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
@@ -43,4 +38,26 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    //make an array
+    var arr = []
+    //repeat num times
+    for(var i = 0; i <= num; i++){
+        //get random color and push into arr
+        arr.push(randomColor())
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor(){
+    //pick a "red" from 0 -255
+    var r = Math.floor(Math.random() * 256);
+    //pick a "green" from 0 -255
+    var g = Math.floor(Math.random() * 256);
+    //pick a "blue" from 0 -255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
