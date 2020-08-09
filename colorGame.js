9e1b8c991b696c9e8c69c491fbbaab4292035a20
미현 <douob0@naver.com>
2020-08-09 20:55:39 +0900

    Change h1 color to match the answer

--- colorGame.js
+++ colorGame.js
@@ -4,6 +4,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,6 +21,7 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }else{
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again"
@@ -46,7 +48,7 @@ function generateRandomColors(num){
     //repeat num times
     for(var i = 0; i <= num; i++){
         //get random color and push into arr
-        arr.push(randomColor())
+        arr.push(randomColor());
     }
     //return that array
     return arr;
