090b4db7112c11e7270fd41a358e0cf4918d7036
미현 <douob0@naver.com>
2020-08-09 21:25:45 +0900

    Create easy and hard mode button
    make it look selected

--- colorGame.js
+++ colorGame.js
@@ -6,6 +6,18 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
 
 resetButton.addEventListener("click", function(){
     //generate all new colors
