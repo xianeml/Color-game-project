7d29c9253736c5808348671dc000c02634eee2d2
미현 <douob0@naver.com>
2020-08-09 19:32:38 +0900

    Add eventListener to every squares

--- colorGame.js
+++ colorGame.js
@@ -13,5 +13,18 @@ var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function(){
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        //compare color to picked color
+        if(clickedColor === pickedColor){
+            alert("Correct!");
+        }else{
+            alert("WRONG!!!!");
+        }
+    });
 }
